1a2162ed9c77a5239d1f9a1d428d8e1534690750
timbradyy <124389347+timbradyy@users.noreply.github.com>
2023-03-09 18:29:30 +0000

    math

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Tim Brady
+// Created on: Feb 2023
 // This file contains the JS functions for index.html
+
+/**
+ * Do basic math.
+ */
+function doMathClicked() {
+  document.getElementById("multiply-math").innerHTML = "<p>5 x 3 = " + (5 * 3) + "</p>"
+  document.getElementById("subtract-math").innerHTML = "<p>7 - 3 =  " + (7 - 3) + "</p>"  
+}
